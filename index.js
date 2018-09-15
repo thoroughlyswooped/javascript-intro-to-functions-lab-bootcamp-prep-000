503449abf24ef7d4222a2655d82bfbcb2dd62693
Ezra T Huffman <joshooha@gmail.com>
2018-09-15 19:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ function logWhisper(string) {
   console.log(`${string}`);
 }
 function sayHiToGrandma (string) {
-  if (string === string.toLowerCase()) {
+  if (string == "I love you, Grandma.") {
+    return "I love you, too.";
+  } else if (string === string.toLowerCase()) {
     return "I can't hear you!"
+  } else if (string === string.toUpperCase){
+    return "YES INDEED!"
   }
 }
