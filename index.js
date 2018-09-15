7b248264e78b87207f96e81ea7ac7c6cd095e646
Ezra T Huffman <joshooha@gmail.com>
2018-09-15 19:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function logShout(string) {
   string = string.toUpperCase();
   console.log(`${string}`)
 }
+function logWhisper(string) {
+  string = string.toLowerCase();
+  console.log(`${string}`);
+}
